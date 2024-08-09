14f01cd0a20c8cd40ffbf3b138e5148968016b3d
Danielle <glen.galit@betterask.erni>
2024-08-09 18:25:49 +0800

    first initial commit

--- CalculatorApp/Calculator.cs
+++ CalculatorApp/Calculator.cs
@@ -5,6 +5,30 @@ public class Calculator
     public double PerformOperation(double num1, double num2, string operation)
     {
         // TODO: Implement the PerformOperation method
-        throw new NotImplementedException();
+
+        switch (operation)
+        {
+            case "add":
+            case "+":
+                return num1 + num2;
+               
+            case "subtract":
+            case "-":
+                return num1 - num2;
+               
+            case "multiply":
+            case "*":
+                return num1 * num2;
+               
+            case "divide":
+            case "/":
+                return num1 / num2;
+               
+            default:
+                throw new InvalidOperationException("Invalid operation.");
+        }
+
+        // throw new NotImplementedException();
     }
+    
 }
